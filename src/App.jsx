7006398b06420d594aa84286065df81bfc5432dd
Programmer-Dev125 <programmerdev125@gmail.com>
2026-01-30 21:27:32 +0500

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -33,15 +33,26 @@ export default function App(){
             <Route path="/policy" element={<Policy />} />  
             <Route path="/flights" element={<Flight />}  /> 
             <Route path="/admin" element={<Login />} />
-            <Route path="/admin/dashboard" element={<Dashboard />} />
-            <Route path="/admin/dashboard/visitors" element={<Dashboard />} />
+            <Route path="/admin/dashboard" element={<Dashboard />}>
+                <Route index element={<DashboardHome />} />
+                <Route path="/visitors" element={<Dashboard />} />
+                <Route path="/bookings" element={<Dashboard />} />
+                <Route path="/hotels" element={<Dashboard />} />
+                <Route path="/messages" element={<Dashboard />} />
+                <Route path="/pending-queries" element={<Dashboard />} />
+                <Route path="/analytics" element={<Dashboard />} />
+                <Route path="/revenue" element={<Dashboard />} />
+                <Route path="/settings" element={<Dashboard />} />
+                <Route path="*" element={<Error />} />
+            </Route>
+            {/* <Route path="/admin/dashboard/visitors" element={<Dashboard />} />
             <Route path="/admin/dashboard/bookings" element={<Dashboard />} />
             <Route path="/admin/dashboard/hotels" element={<Dashboard />} />
             <Route path="/admin/dashboard/messages" element={<Dashboard />} />
             <Route path="/admin/dashboard/pending-queries" element={<Dashboard />} />
             <Route path="/admin/dashboard/analytics" element={<Dashboard />} />
             <Route path="/admin/dashboard/revenue" element={<Dashboard />} />
-            <Route path="/admin/dashboard/settings" element={<Dashboard />} />
+            <Route path="/admin/dashboard/settings" element={<Dashboard />} /> */}
             <Route path="*" element={<Error />} />
           </Routes>
         </Layout>
